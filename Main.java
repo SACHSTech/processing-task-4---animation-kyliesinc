3cd3817466943d4fb6085afb5537ba618fe268ce
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-01-10 12:39:57 -0500

    basic sunset and night animation

--- Main.java
+++ Main.java
@@ -7,10 +7,10 @@ import processing.core.PApplet;
  */
 class Main {
   public static void main(String[] args) {
-    
+
     String[] processingArgs = {"MySketch"};
 	  Sketch mySketch = new Sketch();
 	  PApplet.runSketch(processingArgs, mySketch);
   }
-  
-}
+
+} 
